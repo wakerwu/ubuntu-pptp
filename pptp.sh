202330cf7523117e3e8aeedf92f573dd001c9944
wakerwu <wakerwu@qq.com>
2019-01-30 10:57:37 +0800

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -21,16 +21,15 @@ rm -rf /etc/ppp
 apt-get install -y ppp
 apt-get install -y pptpd
 apt-get install -y iptables logrotate tar cpio perl
-rm -r /dev/ppp
-mknod /dev/ppp c 108 0
 echo 1 > /proc/sys/net/ipv4/ip_forward 
-echo "mknod /dev/ppp c 108 0" >> /etc/rc.local
-echo "echo 1 > /proc/sys/net/ipv4/ip_forward" >> /etc/rc.local
+sed -i "13iiptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"  /etc/rc.local
+sed -i "13i/etc/init.d/pptpd restart"  /etc/rc.local
 echo "localip 11.0.0.10" >> /etc/pptpd.conf
 echo "remoteip 11.0.0.100-200" >> /etc/pptpd.conf
 echo "ms-dns 8.8.8.8" >> /etc/ppp/options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/options
 echo "wakerwu pptpd p19890202 *" >> /etc/ppp/chap-secrets
+echo ""
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables-rules
 printf "
